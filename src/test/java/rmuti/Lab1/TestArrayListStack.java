031746ee4e52f1ba5a2ab57e29908370de1c2fe1
nanthaporn41 <nanthaporn41kk@gmail.com>
2018-01-17 14:42:22 +0700

    kat

---
+++ src/test/java/rmuti/Lab1/TestArrayListStack.java
@@ -0,0 +1,14 @@
+package rmuti.Lab1;
+
+public class TestArrayListStack {
+public static void main(String[]args){
+	ArrayListStack stack = new ArrayListStack();
+	stack.push("macos");
+	stack.push("linux");
+	stack.push("windows");
+System.out.println(stack);;
+String data = (String)stack.pop();
+System.out.println(stack);;
+	}
+
+}
